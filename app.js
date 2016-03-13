6da04a144a1fa4f26dec5dc30b1d0c097dff909a
Christoph G. Vieth <christoph@vieth.me>
2016-03-13 16:54:52 +0100

    I hope nobody will see this ... *SLAP MYSELF*

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ io.on("connection", function (socket) {
 });
 
 setInterval(function () {
-    redis.zrange(keyName, start, stop, 'withscores', function (err, result) {
+    redis.zrange('highscore', '0', '-1', 'withscores', function (err, result) {
         io.sockets.emit('highscore-update', result);
     })
 }, 1000);
